5a48cc2432c8f8f815783865e2a7026746a97441
Vladimir Zabara <wlady2001@gmail.com>
2022-03-23 13:21:18 +0200

    optimized code

--- src/CSV2DB/Models/File.php
+++ src/CSV2DB/Models/File.php
@@ -17,14 +17,14 @@ class File {
 	public static function upload_file() {
 		if ( isset( $_FILES['file'] ) ) {
 			$file_input = $_FILES['file']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-			if ( $file_input['error'] == UPLOAD_ERR_OK ) {
+			if ( sanitize_text_field( $file_input['error'] ) == UPLOAD_ERR_OK ) {
 				$upload_directory = \wp_upload_dir();
 
 				if ( intval( $file_input['size'] ) > self::convert_bytes( ini_get( 'upload_max_filesize' ) ) ) {
 					throw new \Exception( __( 'File size is too big!', 'csv-to-db' ) );
 				}
 
-				switch ( strtolower( __( $file_input['type'] ) ) ) {
+				switch ( strtolower( sanitize_text_field( $file_input['type'] ) ) ) {
 					//allowed file types
 					case 'text/csv':
 					case 'text/plain':
@@ -33,13 +33,13 @@ class File {
 						throw new \Exception( __( 'Unsupported File!', 'csv-to-db' ) );
 				}
 
-				$file_name     = strtolower( __( $file_input['name'] ) );
+				$file_name     = strtolower( sanitize_text_field( $file_input['name'] ) );
 				$file_ext      = substr( $file_name, strrpos( $file_name, '.' ) ); //get file extention
 				$random_number = wp_rand( 0, 9999999999 ); //Random number to be added to name.
 				$new_file_name = $random_number . $file_ext; //new file name
 				$tmp_file_name = $upload_directory['basedir'] . '/' . $new_file_name;
 
-				if ( move_uploaded_file( __( $file_input['tmp_name'] ), $tmp_file_name ) ) {
+				if ( move_uploaded_file( sanitize_text_field( $file_input['tmp_name'] ), $tmp_file_name ) ) {
 					return $tmp_file_name;
 				} else {
 					throw new \Exception( __( 'Error uploading File!', 'csv-to-db' ) );
